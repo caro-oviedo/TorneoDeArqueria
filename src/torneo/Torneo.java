4181159076d88085e73ef0958084d6ce97464566
caro-oviedo <carolina.u.oviedo@gmail.com>
2021-09-21 20:02:10 -0300

    update

--- src/torneo/Torneo.java
+++ src/torneo/Torneo.java
@@ -1,5 +1,28 @@
 package torneo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Torneo {
+	
+	private Map<Integer, PriorityQueue<Integer>> participantes;
+	private TreeMap <Integer, LinkedList<Integer>> podio;
+	
+	public Torneo(String archivo) throws FileNotFoundException {
+		setParticipantes(archivo);
+		setPodio();
+	}
+	
+	private void setParticipantes(String archivo) throws FileNotFoundException{
+		participantes = new TreeMap<Integer, PriorityQueue<Integer>>();
+		Scanner sc = new Scanner (new File (archivo));
+		
+	}
+	
 
 }
